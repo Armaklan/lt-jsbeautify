8182634f69b0a3de6e054f9431928528649e742b
Armaklan <lionel.zuber@armaklan.org>
2014-11-22 08:36:35 +0100

    docs() - Add some comments

--- jsbeautify_compiled.js
+++ jsbeautify_compiled.js
@@ -16,11 +16,11 @@ goog.require('lt.objs.editor');
 goog.require('lt.objs.files');
 goog.require('lt.object');
 lt.plugins.jsbeautify.beautify_path = lt.objs.files.join.call(null,lt.objs.plugins._STAR_plugin_dir_STAR_,"node_modules/js-beautify");
-lt.plugins.jsbeautify.beauty = (function beauty(obj_id,beautify_path,code){var jsbeautify = require(beautify_path).js_beautify;return jsbeautify.call(null,code);
+lt.plugins.jsbeautify.beautify_execution = (function beautify_execution(obj_id,beautify_path,code){var jsbeautify = require(beautify_path).js_beautify;return jsbeautify.call(null,code);
 });
-lt.plugins.jsbeautify.beautify_selection = (function beautify_selection(obj_id,beautify_path){return lt.objs.editor.replace_selection.call(null,obj_id,lt.plugins.jsbeautify.beauty.call(null,obj_id,beautify_path,lt.objs.editor.selection.call(null,obj_id)));
+lt.plugins.jsbeautify.beautify_selection = (function beautify_selection(obj_id,beautify_path){return lt.objs.editor.replace_selection.call(null,obj_id,lt.plugins.jsbeautify.beautify_execution.call(null,obj_id,beautify_path,lt.objs.editor.selection.call(null,obj_id)));
 });
-lt.plugins.jsbeautify.beautify_file = (function beautify_file(obj_id,beautify_path){var position = lt.objs.editor.__GT_cursor.call(null,obj_id);lt.objs.editor.set_val.call(null,obj_id,lt.plugins.jsbeautify.beauty.call(null,obj_id,beautify_path,lt.objs.editor.__GT_val.call(null,obj_id)));
+lt.plugins.jsbeautify.beautify_file = (function beautify_file(obj_id,beautify_path){var position = lt.objs.editor.__GT_cursor.call(null,obj_id);lt.objs.editor.set_val.call(null,obj_id,lt.plugins.jsbeautify.beautify_execution.call(null,obj_id,beautify_path,lt.objs.editor.__GT_val.call(null,obj_id)));
 return lt.objs.editor.move_cursor.call(null,obj_id,position);
 });
 lt.object.behavior_STAR_.call(null,new cljs.core.Keyword("lt.plugins.jsbeautify","on-save","lt.plugins.jsbeautify/on-save",1671963816),new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"save","save",1017427183),null], null), null),new cljs.core.Keyword(null,"type","type",1017479852),new cljs.core.Keyword(null,"user","user",1017503549),new cljs.core.Keyword(null,"desc","desc",1016984067),"JsBeautify: Beautify on save",new cljs.core.Keyword(null,"reaction","reaction",4441361819),(function (this$){var temp__4126__auto__ = lt.objs.editor.pool.last_active.call(null);if(cljs.core.truth_(temp__4126__auto__))
--- src/lt/plugins/jsbeautify.cljs
+++ src/lt/plugins/jsbeautify.cljs
@@ -9,26 +9,30 @@
   (:require-macros [lt.macros :refer [defui behavior]]))
 
 
-;; Define path to js-beautify
+;; Path to BeautifyJs node module
 (def beautify-path (files/join plugins/*plugin-dir* "node_modules/js-beautify"))
 
-(defn beauty [obj-id beautify-path code]
+;; Require Beautify Function from node module
+(defn beautify-execution [obj-id beautify-path code]
     (let [jsbeautify ( .-js_beautify (js/require beautify-path))]
           (jsbeautify code)
       )
 )
 
+;; Apply beautify on current selection
 (defn beautify-selection [obj-id beautify-path]
-  (editor/replace-selection obj-id (beauty obj-id beautify-path (editor/selection obj-id)))
+  (editor/replace-selection obj-id (beautify-execution obj-id beautify-path (editor/selection obj-id)))
  )
 
+;; Apply beautify en current save
 (defn beautify-file [obj-id beautify-path]
   (let [position (editor/->cursor obj-id)]
-      (editor/set-val obj-id (beauty obj-id beautify-path (editor/->val obj-id)))
+      (editor/set-val obj-id (beautify-execution obj-id beautify-path (editor/->val obj-id)))
       (editor/move-cursor obj-id position)
   )
  )
 
+;; Declare on save behaviors to beautify file
 (object/behavior* ::on-save
                   :triggers #{:save}
                   :type :user
@@ -38,12 +42,14 @@
                                (beautify-file ed beautify-path )))                              )
 
 
+;; Declare command to beautify current selection
 (cmd/command {:command :jsbeautify.beautify-selection
               :desc "JsBeautify: Beautify current selection"
               :exec (fn [this]
                       (when-let [ed (pool/last-active)]
                         (beautify-selection ed beautify-path)))})
 
+;; Declare command to beautify js file
 (cmd/command {:command :jsbeautify.beautify-file
               :desc "JsBeautify: Beautify current file"
               :exec (fn [this]
